e4432fe07771fda0c2027010d87296cd328c8281
vimniky <vimniky@gmail.com>
2019-02-10 21:15:55 +0800

    CreateFramework

---
+++ CreateFramework/CreateFramework/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CreateFrameword
+//
+//  Created by viky on 2/8/19.
+//  Copyright © 2019 viky. All rights reserved.
+//
+
+import UIKit
+import MyFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let swift: ProgrammingLanuages = .swift
+        print(swift)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CreateFramework/MyFramework/MyPublickFrameworkApi.swift
@@ -0,0 +1,13 @@
+//
+//  MyPublickFrameworkApi.swift
+//  MyFramework
+//
+//  Created by viky on 2/8/19.
+//  Copyright © 2019 viky. All rights reserved.
+//
+
+import Foundation
+
+public enum ProgrammingLanuages  {
+    case swift, scheme, javascript, c
+}
