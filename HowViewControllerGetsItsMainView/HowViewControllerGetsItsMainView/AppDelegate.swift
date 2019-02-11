1b0d2dccc22e332482a715ac71fb85a228e960a3
vimniky <vimniky@gmail.com>
2019-02-11 20:23:29 +0800

    HowViewControllerGetsItsMainView

--- HowViewControllerGetsItsMainView/HowViewControllerGetsItsMainView/AppDelegate.swift
+++ HowViewControllerGetsItsMainView/HowViewControllerGetsItsMainView/AppDelegate.swift
@@ -6,6 +6,19 @@
 //  Copyright © 2019 viky. All rights reserved.
 //
 
+
+
+//1. When the view controller first decides that it needs its view, loadView is always called:
+//• If we override loadView, we supply and set the view in code, and we do not call super. Therefore, the process of seeking a view comes to an end.
+//• If we don’t override loadView, UIViewController’s built-in default imple‐ mentation of loadView takes over, and performs the rest of the process.
+//2. UIViewController’s default implementation of loadView looks for a nib:
+//• If the view controller was instantiated with an explicit nibName:, a nib with that name is sought, and the process comes to an end. (If there is no such nib, the app will crash at launch.)
+//• If the view controller was instantiated with a nil nibName:, then:
+//1. An eponymous nib is sought. If it is found, it is loaded and the process
+//comes to an end.
+//2. If the view controller’s name ends in “Controller,” an eponymous nib without the “Controller” is sought. If it is found, it is loaded and the process comes to an end.
+//3. If we reach this point, UIViewController’s default implementation of loadView creates a generic UIView.
+
 import UIKit
 
 @UIApplicationMain
@@ -13,34 +26,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = window ?? UIWindow()
+        // let rv = RootViewController()
+        
+        // Load from nib
+        // Don't forget to connect the File’s Owner view outlet to the View object.
+        
+        // let rv = RootViewController(nibName: "ViewFromNib", bundle: nil)
+        // let rv = RootViewController()
+        let rv = WithXibFileController()
+        window?.rootViewController = rv
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
+
+
 
--- HowViewControllerGetsItsMainView/HowViewControllerGetsItsMainView/RootViewController.swift
+++ HowViewControllerGetsItsMainView/HowViewControllerGetsItsMainView/RootViewController.swift
@@ -10,21 +10,38 @@ import UIKit
 
 class RootViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    fileprivate func populateView() {
+        let v = UIView()
+        v.backgroundColor = .green
+        self.view = v
+        let label = UILabel()
+        v.addSubview(label)
+        label.text = "Hello, World!"
+        label.autoresizingMask = [
+            .flexibleTopMargin,
+            .flexibleLeftMargin,
+            .flexibleBottomMargin,
+            .flexibleRightMargin]
+        label.sizeToFit()
+        label.center = CGPoint(x: v.bounds.midX, y: v.bounds.midY)
+        label.frame = label.frame.integral
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // override func loadView() {
+        // When we created our view controller’s view (self.view), we never gave it a reason‐ able frame. This is because we are relying on someone else to frame the view appro‐ priately. In this case, the “someone else” is the window, which responds to having its rootViewController property set to a view controller by framing the view control‐ ler’s view appropriately as the root view before putting it into the window as a sub‐ view. In general, it is the responsibility of whoever puts a view controller’s view into the interface to give the view the correct frame — and this will never be the view con‐ troller itself
+        
+        // Indeed, the size of a view controller’s view may be changed as it is placed into the interface, and you must keep in mind, as you design your view controller’s view and its subviews, that this can happen
+        
+        // If we don’t implement loadView, and if no view is supplied in any other way, then UIViewController’s default implementation of loadView will do exactly what we are doing: it creates a generic UIView object and assigns it to self.view.
+        
+         // populateView()
+    // }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // A more appropriate place to populate a view controller’s view is its view- DidLoad implementation, which is called after the view exists and can be referred to as self.view.
+        
+        // populateView()
     }
-    */
-
 }
