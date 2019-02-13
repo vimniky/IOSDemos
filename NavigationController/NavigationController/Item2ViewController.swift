94ab3ab6791642de45f321ec64898fc0bbf25e97
vimniky <vimniky@gmail.com>
2019-02-13 14:57:33 +0800

    NavigationController

---
+++ NavigationController/NavigationController/Item2ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Item2ViewController.swift
+//  NavigationController
+//
+//  Created by viky on 2/12/19.
+//  Copyright © 2019 viky. All rights reserved.
+//
+
+import UIKit
+
+class Item2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction override func unwind(for unwindSegue: UIStoryboardSegue, towards subsequentVC: UIViewController) {
+        //
+    }
+
+}
---
+++ NavigationController/NavigationController/UnwindDemoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UnwindDemoViewController.swift
+//  NavigationController
+//
+//  Created by viky on 2/12/19.
+//  Copyright © 2019 viky. All rights reserved.
+//
+
+import UIKit
+
+class UnwindDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
